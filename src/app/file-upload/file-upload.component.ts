c9c2d1314aa4a1fda318ba6b60058cb44525bc83
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-16 09:10:21 +0800

    Drop Zone Component Logic

---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml'];
+
+@Component({
+  selector: 'app-file-upload',
+  imports: [CommonModule],
+  templateUrl: './file-upload.component.html',
+  styleUrl: './file-upload.component.css',
+})
+export class FileUploadComponent {
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+
+  allowedFileTypes = ALLOWED_FILE_TYPES;
+
+  isUploading = false;
+  fileUrl!: string | null;
+  uploadFile!: File | null;
+
+  handleChange(event: any) {
+    const file = event.target.files[0] as File;
+    this.fileUrl = URL.createObjectURL(file);
+    this.uploadFile = file;
+  }
+
+  handleRemovesFile() {
+    if (this.fileInput && this.fileInput.nativeElement) {
+      this.fileInput.nativeElement.value = null;
+    }
+
+    this.uploadFile = null;
+    this.fileUrl = null;
+  }
+
+  handleUploadFile() {
+    // logic to upload file
+  }
+}
